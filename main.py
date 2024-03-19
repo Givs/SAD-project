cc0fac8121c84a6538447bdc92fe3c9e32c759ce
Givaldo Neto <valdingivs@outlook.com>
2024-03-18 23:05:20 -0300

    :recycle: add blank lines

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from weather_api import get_weather_info
 from materials import recommend_materials
 
-api_key = 'b70f41432f7ca03f275efffde38b7e96'  # Substitua pela sua chave de API real
+api_key = 'b70f41432f7ca03f275efffde38b7e96'
 city_name = 'Aracaju'
 
 weather_info = get_weather_info(city_name, api_key)
 if 'error' not in weather_info:
     materials_recommended = recommend_materials(weather_info)
+
     print(f"Para a cidade de {city_name}, os seguintes materiais de construção são recomendados:")
+
     for material in materials_recommended:
         print(f"- {material}")
 else:
--- weather_api.py
+++ weather_api.py
@@ -14,11 +14,14 @@ def get_weather_info(city_name, api_key):
     response = requests.get(base_url, params=params)
     if response.status_code == 200:
         data = response.json()
+
         wind_speed_kmh = data['wind']['speed'] * 3.6
+
         return {
             'humidity': data['main']['humidity'],
             'temperature': data['main']['temp'],
             'wind_speed': wind_speed_kmh
         }
+
     else:
         return {'error': 'Failed to retrieve weather information'}
